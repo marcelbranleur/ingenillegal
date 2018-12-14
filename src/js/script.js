d59676d8f5a2a7d2c8e628254ce4df7b45108d51
Marcel <marcel.branleur@gmail.com>
2018-12-14 20:44:08 +0100

    start

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('#menu-toggle, #menu > ul > li > a').click(function(e) {
+        var $toggle = $(this);
+        var $menu = $('#' + $(this).attr('aria-controls'));
+
+        if ($menu.attr('aria-hidden') == 'true') {
+            $('body').addClass('open');
+            $menu.attr('aria-hidden', 'false');
+            $toggle.attr('aria-expanded', 'true');
+
+        }
+        else if ($menu.attr('aria-hidden') == 'false') {
+            $('body').removeClass('open');
+            $menu.attr('aria-hidden', 'true');
+            $toggle.attr('aria-expanded', 'faremove');
+        }
+    });
+
+    $('#close, #menu > ul > li > a').click(function(e) {
+        var $toggle = $(this);
+        var $menu = $('#menu');
+
+
+         if ($menu.attr('aria-hidden') == 'false') {
+            $('body').removeClass('open');
+            $menu.attr('aria-hidden', 'true');
+            $toggle.attr('aria-expanded', 'faremove');
+        }
+    });
+});
+
+
