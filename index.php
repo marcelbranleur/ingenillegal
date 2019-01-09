0d480e25fe8df8ce642699d7fd38d9c5a2f6540d
Alvine <alvine@tamkin.se>
2019-01-09 16:21:12 +0100

    markup for news page

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="container">
 
 		<?php
 		if ( have_posts() ) :
@@ -24,9 +25,9 @@ get_header();
 			if ( is_home() && ! is_front_page() ) :
 				// TODO: What is this?
 				?>
-				<header>
+				<div class="entry-header">
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				</div>
 				<?php
 			endif;
 
@@ -39,7 +40,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', 'excerpt' );
 
 			endwhile;
 
@@ -51,9 +52,9 @@ get_header();
 
 		endif;
 		?>
-
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php 
+<?php
 get_footer();
